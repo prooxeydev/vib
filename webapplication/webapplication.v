f93e1a2689acc4067aee7c2d4eab02a89639eb54
louis <dev@louis-schmieder.de>
2020-05-10 23:35:05 +0200

    Started webapplication

--- webapplication/webapplication.v
+++ webapplication/webapplication.v
@@ -1,5 +1,19 @@
 module main
 
-fn main() {
-	println('Hello World !')
+import vweb
+
+pub struct Vib{
+mut:
+	vweb vweb.Context
 }
+
+fn main() {
+	vweb.run<Vib>(8080)
+}
+
+fn (vib mut Vib) index() {
+	app.vweb.text('Hello world from vweb!')
+}
+
+pub fn (vlib &App) init() {}
+pub fn (vlib &App) reset() {}
