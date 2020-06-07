a980c11a9c50f729feb09313e9270ed53bab46cf
louis <dev@louis-schmieder.de>
2020-06-07 16:40:22 +0200

    Make webapplication compilable

--- webapplication/webapplication.v
+++ webapplication/webapplication.v
@@ -1,19 +1,45 @@
 module main
 
 import vweb
+import vweb.assets
+import os
 
-pub struct Vib{
+const (
+	port = 8082
+)
+
+struct Vib {
 pub mut:
 	vweb vweb.Context
+	static_css []byte
+	static_js []byte
 }
 
 fn main() {
-	vweb.run<Vib>(8080)
+	vweb.run<Vib>(port)
 }
 
-fn (mut vib Vib) index() {
-	vib.vweb.text('Hello world from vweb!')
+pub fn (mut app Vib) index() {
+	mut site_css := get_css('./assets/userpage.css')
+	site_css << app.static_css
+	css := string(site_css)
+	$vweb.html()
 }
 
-pub fn (vlib Vib) init() {}
-pub fn (vlib Vib) reset() {}
+pub fn (mut app Vib) init() {
+	static_css := get_css('./assets/footer.css', './assets/header.css', './assets/main.css')
+	app.static_css = static_css
+}
+
+pub fn (mut app Vib) reset() {
+
+}
+
+fn get_css(css ...string) []byte {
+	mut d_css := []byte{}
+	for file in css {
+		data := os.read_file(file) or { '' }
+		d_css << assets.minify_css(data).bytes()
+	}
+	return d_css
+}
